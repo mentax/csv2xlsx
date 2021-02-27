cf45e8c78cf4b605097319cbd6e1dd1be7ab5de6
Dawid Polak <dawid.polak@mentax.pl>
2021-02-28 00:15:33 +0100

    test

--- example/run.sh
+++ example/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-./csv2xlsx -t template.xlsx -s Sh1 -s NewSheet -r 4 -o out.xlsx databig.csv data.csv
+../csv2xlsx -t template.xlsx -s Sh1 -s NewSheet -r 4 -o out.xlsx databig.csv data.csv
